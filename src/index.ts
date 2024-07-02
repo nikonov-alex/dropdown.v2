25b1aa8b9d5244682ae5eafa150bf77f033813e1
Александр <alex@debian.loc>
2024-07-02 20:25:18 +0300

    options

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { FormComponent } from "@nikonov-alex/components.v2";
 import { State as DropdownState, dropdown } from "./dropdown";
 
+type Options = {
+    dommode?: "light" | "shadow",
+    styles?: CSSStyleSheet
+}
+
 class Dropdown extends FormComponent<DropdownState, { value: string }> {
 
-    constructor( styles?: CSSStyleSheet ) {
-        super( dropdown, styles );
+    constructor( options: Options ) {
+        super( {
+            ... dropdown,
+            dommode: options.dommode || "shadow"
+        }, options.styles );
     }
 
 }
